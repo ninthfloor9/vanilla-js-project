f5265ff072765f9b1f4d97f494ae329ab14c0b36
jjeong <jjeong@riotgames.com>
2023-06-09 17:41:58 +0900

    update

---
+++ js-doc/browser/browser.js
@@ -0,0 +1,12 @@
+// getElement, querySelector 로 요소 검색하기
+
+//getElementById html 태그의 id를 참조한다.
+
+let ddabong = document.getElementById('ddabong'); // id = ddabong 으로 dom 요소 지정하고
+ddabong.style.background = 'yellow'; // 이 dom 요소의 style의 backgound color를 yellow
+ddabong.style.textAlign = 'center'; // 이 dom 요소의 style의 textAlign 을 center로;
+
+// querySelectorAll 자식요소중 주어진 CSS 선택자에 대응하는 요소 모두를 반환한다.
+
+
+
